8ee29199b66290bd7734ccd5e1fba6365b916bc9
David Whaley <davidwhaleycode@outlook.com>
2019-02-15 22:21:56 -0600

    filter template scaffold

--- react-real-estate/public/js/components/realEstate.js
+++ react-real-estate/public/js/components/realEstate.js
@@ -43,7 +43,7 @@ var Filter = function (_Component) {
     value: function render() {
       return _react2.default.createElement(
         'section',
-        { className: 'filter' },
+        { id: 'filter' },
         _react2.default.createElement(
           'h4',
           null,
@@ -51,7 +51,7 @@ var Filter = function (_Component) {
         ),
         _react2.default.createElement(
           'select',
-          { name: 'neighborhood', className: 'neighborhood' },
+          { name: 'neighborhood', className: 'filters neighborhood' },
           _react2.default.createElement(
             'option',
             null,
@@ -60,7 +60,7 @@ var Filter = function (_Component) {
         ),
         _react2.default.createElement(
           'select',
-          { name: 'housetype', className: 'housetype' },
+          { name: 'housetype', className: 'filters housetype' },
           _react2.default.createElement(
             'option',
             null,
@@ -69,11 +69,77 @@ var Filter = function (_Component) {
         ),
         _react2.default.createElement(
           'select',
-          { name: 'bedrooms', className: 'bedrooms' },
+          { name: 'bedrooms', className: 'filters bedrooms' },
           _react2.default.createElement(
             'option',
             null,
             '2 BR'
+          )
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'filters price' },
+          _react2.default.createElement(
+            'span',
+            { className: 'title' },
+            'Price'
+          ),
+          _react2.default.createElement('input', { type: 'text', name: 'min-price', className: 'min-price' }),
+          _react2.default.createElement('input', { type: 'text', name: 'max-price', className: 'max-price' })
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'filters floor-space' },
+          _react2.default.createElement(
+            'span',
+            { className: 'title' },
+            'Floor-Space'
+          ),
+          _react2.default.createElement('input', { type: 'text', name: 'min-floor-space', className: 'min-floor-space' }),
+          _react2.default.createElement('input', { type: 'text', name: 'max-floor-space', className: 'max-floor-space' })
+        ),
+        _react2.default.createElement(
+          'div',
+          { className: 'filters extras' },
+          _react2.default.createElement(
+            'label',
+            { 'for': 'extras' },
+            _react2.default.createElement(
+              'span',
+              null,
+              'Elevators'
+            ),
+            _react2.default.createElement('input', { name: 'extras', value: 'elevator', type: 'checkbox' })
+          ),
+          _react2.default.createElement(
+            'label',
+            { 'for': 'extras' },
+            _react2.default.createElement(
+              'span',
+              null,
+              'Swimming Pool'
+            ),
+            _react2.default.createElement('input', { name: 'extras', value: 'swimming-pool', type: 'checkbox' })
+          ),
+          _react2.default.createElement(
+            'label',
+            { 'for': 'extras' },
+            _react2.default.createElement(
+              'span',
+              null,
+              'Finished Basement'
+            ),
+            _react2.default.createElement('input', { name: 'extras', value: 'finished basement', type: 'checkbox' })
+          ),
+          _react2.default.createElement(
+            'label',
+            { 'for': 'extras' },
+            _react2.default.createElement(
+              'span',
+              null,
+              'Gym'
+            ),
+            _react2.default.createElement('input', { name: 'extras', value: 'gym', type: 'checkbox' })
           )
         )
       );
