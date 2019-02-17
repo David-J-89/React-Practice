4f647be870c94abf6b696ba2b5a23f2849d8cf96
David Whaley <davidwhaleycode@outlook.com>
2019-02-16 18:43:31 -0600

    pet shop seed data state complete

---
+++ react-pets/assets/js/petsApp/data/petslistData.js
@@ -0,0 +1,145 @@
+var petslistData = [
+    {
+        petType: 'Fish',
+        petBreed: 'Tetra',
+        petName: 'Lucy',
+        petSize: 'Small',
+        petColor: 'Blue',
+        petSex: 'Female',
+        petPrice: 10.00,
+        
+        petRequirements: [
+            'Water',
+            'Aquarium',
+            'Fish Food'
+        ],        
+        image:
+        'https://i.pinimg.com/736x/7b/54/4d/7b544d96a8bc413dc8b46079fab1f65e.jpg'
+    },
+    {
+        petType: 'Fish',
+        petBreed: 'Beta',
+        petName: 'Frank',
+        petSize: 'Small',
+        petColor: 'Pink',
+        petSex: 'Male',
+        petPrice: 25.00,
+        
+        petRequirements: [
+            'Water',
+            'Aquarium',
+            'Fish Food'
+        ],        
+        image:
+        'https://i.pinimg.com/originals/bf/64/26/bf642601bd531187f94bdc0e82dd8392.jpg'
+    },
+    {
+        petType: 'Dog',
+        petBreed: 'Husky',
+        petName: 'Cherry',
+        petSize: 'Large',
+        petColor: 'Grey/White',
+        petSex: 'Female',
+        petPrice: 150.00,
+
+        petRequirements: [
+            'Water',
+            'Grooming',
+            'Exercise',
+            'Dog Food'
+        ],        
+        image:
+        'https://dogdiscoveries.com/wp-content/uploads/2016/02/husky-grey-and-white.png'
+    },
+    {
+        petType: 'Bird',
+        petBreed: 'Cockatoo',
+        petName: 'Mara',
+        petSize: 'Small',
+        petColor: 'White',
+        petSex: 'Female',
+        petPrice: 80.00,
+        
+        petRequirements: [
+            'Water',
+            'Bird-Food',
+            'Cage'
+        ],        
+        image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Citron-crested_Cockatoo.jpg/407px-Citron-crested_Cockatoo.jpg'
+    },
+    {
+        petType: 'Reptile',
+        petBreed: 'Iquana',
+        petName: 'Dave',
+        petSize: 'Medium',
+        petColor: 'Green',
+        petSex: 'Male',
+        petPrice: 75.00,
+
+        
+        petRequirements: [
+            'Water',
+            'Heat'
+            'Iguana Food'
+        ],        
+        image:
+        'http://4.bp.blogspot.com/_wviAfk7xoho/TKahcOunRrI/AAAAAAAAADo/poN8DKTy5J0/s1600/green-iguana2.jpg'
+    },
+    {
+        petType: 'Reptile',
+        petBreed: 'Tortoise',
+        petName: 'Nalli',
+        petSize: 'Large',
+        petColor: 'Grey',
+        petSex: 'Female',
+        petPrice: '240.00',
+        
+        petRequirements: [
+            'Water',
+            'Tortoise Food'
+        ],        
+        image:
+        'http://1.bp.blogspot.com/-Q-hsHJgLvUE/UN-sCDYP8RI/AAAAAAAAAg0/YUpE0i-Ph_Q/s1600/Aldabra+Giant+Tortoise.jpg'
+    },
+    {
+        petType: 'Cat',
+        petBreed: 'Siamese',
+        petName: 'Alphonso',
+        petSize: 'Medium',
+        petColor: 'Black/Grey',
+        petSex: 'Male',
+        petPrice: '65.00'
+        
+        petRequirements: [
+            'Water',
+            'Cat Food',
+            'Litter Box'
+        ],        
+        image:
+        'https://i.pinimg.com/736x/7b/54/4d/7b544d96a8bc413dc8b46079fab1f65e.jpg'
+    },
+    {
+        petType: 'Cat',
+        petBreed: '',
+        petName: 'Lucy',
+        petSize: 'small',
+        petColor: 'blue',
+        petSex: 'Female',
+        petPrice: 92.00
+        
+        petRequirements: [
+            'Water',
+            'Cat Food',
+            'Litter Box'
+        ],        
+        image:
+        'http://1.bp.blogspot.com/-N7FFMnsQTwM/UJ86C9tjwNI/AAAAAAAABMs/RVUSPMds4Vo/s1600/Korat_Cat_Picture.jpg'
+    },
+    
+    
+    
+    
+]
+
+export default petslistData;
